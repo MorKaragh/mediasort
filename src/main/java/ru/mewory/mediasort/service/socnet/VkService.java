741de0ba548782d077661d512fd9f75887bb3a49
MorKaragh <hentai.exe@gmail.com>
2019-10-02 09:50:15 +0300

    UserActor expiration

--- src/main/java/ru/mewory/mediasort/service/socnet/VkService.java
+++ src/main/java/ru/mewory/mediasort/service/socnet/VkService.java
@@ -35,29 +35,33 @@ import java.util.*;
 @PropertySource("classpath:application.properties")
 public class VkService {
 
-    public static final long TIMEOIT = 200L;
+    private static final long TIME_WAIT_BEFORE_REQUEST = 200L;
+    private static final int POSTS_BATCH_SIZE = 10;
+    private Logger logger = LoggerFactory.getLogger(VkService.class);
+
     @Value("${vk.appid}")
     private Integer APP_ID;
+
     @Value("${vk.postauthor}")
     private String POST_AUTHOR;
+
     @Value("${vk.clientsecret}")
     private String CLIENT_SECRET;
+
     @Value("${vk.code}")
     private String CODE;
-    Logger logger = LoggerFactory.getLogger(VkService.class);
 
-    private static final int COUNT = 10;
     @Value("${self.domain}")
     private String selfDomain;
 
-    private TransportClient transportClient;
     private VkApiClient vk;
     private ServiceActor actor;
-    private String authcode = "99b697be370af2b968";
+    private UserActor userActor;
+    private Long userActorExpiresAt;
 
     @PostConstruct
-    public void postConstruct() throws ClientException, ApiException {
-        transportClient = HttpTransportClient.getInstance();
+    public void postConstruct() {
+        TransportClient transportClient = HttpTransportClient.getInstance();
         vk = new VkApiClient(transportClient);
         actor = new ServiceActor(APP_ID, CLIENT_SECRET, CODE);
     }
@@ -74,7 +78,7 @@ public class VkService {
         Integer ownerId = andreyvorobiev.get(0).getId();
         GetResponse posts = vk.wall().get(actor)
                 .ownerId(ownerId)
-                .count(COUNT)
+                .count(POSTS_BATCH_SIZE)
                 .offset(offset)
                 .filter(WallGetFilter.OWNER)
                 .execute();
@@ -85,97 +89,101 @@ public class VkService {
     private List<List<SocnetDTO>> loadPosts(Integer ownerId, List<WallpostFull> items) throws InterruptedException, ApiException, ClientException {
         List<List<SocnetDTO>> result = new ArrayList<>();
 
-
-        UserAuthResponse authResponse = vk.oauth().userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET,
-                selfDomain + "/vkload", authcode).execute();
-        UserActor userActor = new UserActor(APP_ID, authResponse.getAccessToken());
-
         for (WallpostFull wallpostFull : items) {
             List<SocnetDTO> dtos = new ArrayList<>();
-            SocnetDTO post = new SocnetDTO("andreyvorobiev", wallpostFull.getText());
-            post.setId(Long.valueOf(wallpostFull.getId()));
-            post.setSocnet(SocNet.VK);
-            post.setDate(new Date(wallpostFull.getDate() * 1000L));
-            Thread.sleep(TIMEOIT);
-
-            GetCommentsResponse allComments = vk.wall()
-                    .getComments(actor, wallpostFull.getId())
-                    .sort(WallGetCommentsSort.ASC)
-                    .count(Integer.MAX_VALUE)
-                    .ownerId(ownerId)
-                    .execute();
-
-            dtos.add(post);
-
             Map<Integer, String> users = new HashMap<>();
             Set<String> userIds = new HashSet<>();
             Set<String> groupIds = new HashSet<>();
-            for (WallComment c : allComments.getItems()) {
-                putComment(dtos, userIds, groupIds, c);
-            }
 
-            List<WallpostAttachment> attachments = wallpostFull.getAttachments();
+            extractComments(ownerId, wallpostFull, dtos, userIds, groupIds);
+            extractAttachmentComments(ownerId, wallpostFull, dtos, userIds, groupIds);
+            fillUsernames(dtos, users, userIds, groupIds);
 
-            if (CollectionUtils.isNotEmpty(attachments)) {
-                for (WallpostAttachment a : attachments) {
-//                    if (a.getPhoto() != null && a.getPhoto().getId() != null) {
-//                        Thread.sleep(TIMEOIT);
-//                        com.vk.api.sdk.objects.photos.responses.GetCommentsResponse photoComments
-//                                = vk.photos().getComments(userActor, a.getPhoto().getId()).execute();
-//                        for (WallComment c : photoComments.getItems()) {
-//                            putComment(dtos, userIds, groupIds, c);
-//                        }
-//                    }
-                    if (a.getVideo() != null && a.getVideo().getId() != null) {
-                        Integer comments = a.getVideo().getComments();
-                        int offset = 0;
-                        while (comments > 0) {
-                            Thread.sleep(TIMEOIT);
-                            com.vk.api.sdk.objects.video.responses.GetCommentsResponse videoComments
-                                    = vk.videos()
-                                    .getComments(userActor, a.getVideo().getId())
-                                    .ownerId(ownerId)
-                                    .count(100)
-                                    .offset(offset)
-                                    .execute();
-                            for (WallComment c : videoComments.getItems()) {
-                                putComment(dtos, userIds, groupIds, c);
-                            }
-                            offset += 100;
-                            comments -= 100;
+            result.add(dtos);
+        }
+
+        return result;
+    }
+
+    private void extractComments(Integer ownerId, WallpostFull wallpostFull, List<SocnetDTO> dtos, Set<String> userIds, Set<String> groupIds) throws InterruptedException, ApiException, ClientException {
+        SocnetDTO post = new SocnetDTO("andreyvorobiev", wallpostFull.getText());
+        post.setId(Long.valueOf(wallpostFull.getId()));
+        post.setSocnet(SocNet.VK);
+        post.setDate(new Date(wallpostFull.getDate() * 1000L));
+
+        dtos.add(post);
+
+        Thread.sleep(TIME_WAIT_BEFORE_REQUEST);
+
+        GetCommentsResponse allComments = vk.wall()
+                .getComments(actor, wallpostFull.getId())
+                .sort(WallGetCommentsSort.ASC)
+                .count(Integer.MAX_VALUE)
+                .ownerId(ownerId)
+                .execute();
+
+        for (WallComment c : allComments.getItems()) {
+            putComment(dtos, userIds, groupIds, c);
+        }
+    }
+
+    private void fillUsernames(List<SocnetDTO> dtos, Map<Integer, String> users, Set<String> userIds, Set<String> groupIds) throws ApiException, ClientException, InterruptedException {
+        List<UserXtrCounters> allUsers = null;
+        if (!userIds.isEmpty()) {
+            List<String> userz = new ArrayList<>(userIds);
+            allUsers = vk.users().get(actor).userIds(userz).fields(UserField.SCREEN_NAME).execute();
+        }
+
+        List<GroupFull> execute = null;
+        if (!groupIds.isEmpty()) {
+            List<String> groupz = new ArrayList<>(groupIds);
+            execute = vk.groups().getById(actor).groupIds(groupz).execute();
+        }
+
+        Thread.sleep(TIME_WAIT_BEFORE_REQUEST);
+        if (allUsers != null) {
+            allUsers.forEach(userXtrCounters -> users.put(userXtrCounters.getId()
+                    , userXtrCounters.getFirstName() + " " + userXtrCounters.getLastName()));
+        }
+        if (execute != null) {
+            execute.forEach(groupFull -> users.put(-Integer.valueOf(groupFull.getId()), groupFull.getName()));
+        }
+        for (int i = 1; i < dtos.size(); i++) {
+            SocnetDTO dto = dtos.get(i);
+            dto.setAuthor(users.get(dto.getUserId()));
+        }
+    }
+
+    private void extractAttachmentComments(Integer ownerId, WallpostFull wallpostFull, List<SocnetDTO> dtos, Set<String> userIds, Set<String> groupIds) throws InterruptedException, ApiException, ClientException {
+        if (userActor == null || System.currentTimeMillis() / 1000 >= userActorExpiresAt) {
+            return;
+        }
+
+        List<WallpostAttachment> attachments = wallpostFull.getAttachments();
+
+        if (CollectionUtils.isNotEmpty(attachments)) {
+            for (WallpostAttachment a : attachments) {
+                if (a.getVideo() != null && a.getVideo().getId() != null) {
+                    Integer comments = a.getVideo().getComments();
+                    int offset = 0;
+                    while (comments > 0) {
+                        Thread.sleep(TIME_WAIT_BEFORE_REQUEST);
+                        com.vk.api.sdk.objects.video.responses.GetCommentsResponse videoComments
+                                = vk.videos()
+                                .getComments(userActor, a.getVideo().getId())
+                                .ownerId(ownerId)
+                                .count(100)
+                                .offset(offset)
+                                .execute();
+                        for (WallComment c : videoComments.getItems()) {
+                            putComment(dtos, userIds, groupIds, c);
                         }
-
+                        offset += 100;
+                        comments -= 100;
                     }
                 }
             }
-
-            List<UserXtrCounters> allUsers = null;
-            if (!userIds.isEmpty()) {
-                List<String> userz = new ArrayList<>(userIds);
-                allUsers = vk.users().get(actor).userIds(userz).fields(UserField.SCREEN_NAME).execute();
-            }
-
-            List<GroupFull> execute = null;
-            if (!groupIds.isEmpty()) {
-                List<String> groupz = new ArrayList<>(groupIds);
-                execute = vk.groups().getById(actor).groupIds(groupz).execute();
-            }
-
-            Thread.sleep(TIMEOIT);
-            if (allUsers != null) {
-                allUsers.forEach(userXtrCounters -> users.put(userXtrCounters.getId()
-                        , userXtrCounters.getFirstName() + " " + userXtrCounters.getLastName()));
-            }
-            if (execute != null) {
-                execute.forEach(groupFull -> users.put(-Integer.valueOf(groupFull.getId()), groupFull.getName()));
-            }
-            for (int i = 1; i < dtos.size(); i++) {
-                SocnetDTO dto = dtos.get(i);
-                dto.setAuthor(users.get(dto.getUserId()));
-            }
-            result.add(dtos);
         }
-        return result;
     }
 
     private void putComment(List<SocnetDTO> dtos, Set<String> userIds, Set<String> groupIds, WallComment c) {
@@ -192,6 +200,14 @@ public class VkService {
     }
 
     public void setAuthCode(String code) {
-        authcode = code;
+        try {
+            UserAuthResponse authResponse = vk.oauth().userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET,
+                    selfDomain + "/vkload", code).execute();
+            userActorExpiresAt = System.currentTimeMillis() / 1000 + authResponse.getExpiresIn() - 1;
+            userActor = new UserActor(APP_ID, authResponse.getAccessToken());
+        } catch (ApiException | ClientException e) {
+            logger.error("error while creating UserActor", e);
+        }
+
     }
 }
